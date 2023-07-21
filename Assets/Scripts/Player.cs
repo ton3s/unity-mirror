b54417a3b92fc09eb48b9c3a8fee7ee2ff0c34b7
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-07-20 21:59:12 -0500

    Setup initial networking using client authority.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
+	private void HandleMovement()
+	{
+		if (isLocalPlayer)
+		{
+			float x = Input.GetAxis("Horizontal");
+			float z = Input.GetAxis("Vertical");
+			Vector3 movement = new Vector3(x, 0, z);
+			float moveSpeed = 5f;
+			transform.position += movement * moveSpeed * Time.deltaTime;
+		}
+	}
+
 	private void Update()
 	{
-
+		HandleMovement();
 	}
 }
